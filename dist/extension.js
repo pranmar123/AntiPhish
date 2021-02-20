5752a98e129035a4b4a6065a9a8a11a0363a6017
dashon1 <dashonwwIII@pm.me>
2021-02-19 18:13:54 -0600

    This should finally get gmail.js to load before the page without being blocked by chrome. It should also expose the gmail object simply by using the "gmail" variable inside of your browsers console. No congiguration needed courtesy of our secondary boiler plate code located here "https://github.com/josteink/gmailjs-node-boilerplate".

---
+++ dist/extension.js
@@ -0,0 +1,31 @@
+(function(){function r(e,n,t){function o(i,f){if(!n[i]){if(!e[i]){var c="function"==typeof require&&require;if(!f&&c)return c(i,!0);if(u)return u(i,!0);var a=new Error("Cannot find module '"+i+"'");throw a.code="MODULE_NOT_FOUND",a}var p=n[i]={exports:{}};e[i][0].call(p.exports,function(r){var n=e[i][1][r];return o(n||r)},p,p.exports,r,e,n,t)}return n[i].exports}for(var u="function"==typeof require&&require,i=0;i<t.length;i++)o(t[i]);return o}return r})()({1:[function(require,module,exports){
+"use strict";
+
+// loader-code: wait until gmailjs has finished loading, before triggering actual extensiode-code.
+const loaderId = setInterval(() => {
+    if (!window._gmailjs) {
+        return;
+    }
+
+    clearInterval(loaderId);
+    startExtension(window._gmailjs);
+}, 100);
+
+// actual extension-code
+function startExtension(gmail) {
+    console.log("Extension loading...");
+    window.gmail = gmail;
+
+    gmail.observe.on("load", () => {
+        const userEmail = gmail.get.user_email();
+        console.log("Hello, " + userEmail + ". This is your extension talking!");
+
+        gmail.observe.on("view_email", (domEmail) => {
+            console.log("Looking at email:", domEmail);
+            const emailData = gmail.new.get.email_data(domEmail);
+            console.log("Email data:", emailData);
+        });
+    });
+}
+
+},{}]},{},[1]);
